0c806907b35f5ef3dfb08e46e85c2ba368e37372
Vladimir01082023 <vladimir-malyshev-2014@mail.ru>
2024-04-11 08:00:14 +0300

    Fixing the mistakes

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
@@ -16,7 +17,12 @@ import java.util.stream.Collectors;
 @Service
 @Slf4j
 public class FilmService {
+
+    @Autowired
+    @Qualifier("inMemoryFilmStorage")
     private final InMemoryFilmStorage inMemoryFilmStorage;
+    @Autowired
+    @Qualifier("inMemoryUserStorage")
     private final InMemoryUserStorage inMemoryUserStorage;
 
     @Autowired
