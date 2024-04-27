ccd4f4c140fd8c486c1fdf514288082ce6063151
Vladimir01082023 <vladimir-malyshev-2014@mai.ru>
2024-04-28 00:40:31 +0300

    Version number 2

--- src/main/java/ru/yandex/practicum/filmorate/storage/DAO/UserStorageDAO.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/DAO/UserStorageDAO.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.storage.DAO;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -10,7 +9,6 @@ import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exceptions.UserExistingException;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
